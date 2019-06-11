0cda17a3a023e92fe14e4b34c82b14e2c2d65805
eastend-street <ss_1322irf@icloud.com>
2019-06-11 13:51:00 -0700

    change file name

--- src/GettingAreaWIthInput.java
+++ src/GettingAreaWIthInput.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GettingAreaWIthInput {
+public class GettingAreaWithInput {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Tell me the radius in cm");

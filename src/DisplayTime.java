2413713b35720616f2ed2bf5e9750ed41d30ebbf
eastend-street <ss_1322irf@icloud.com>
2019-06-11 15:38:14 -0700

    made display time program

---
+++ src/DisplayTime.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+/**
+ * @author Jun
+ * @version 1.0
+ *
+ */
+public class DisplayTime {
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer for seconds: ");
+		int seconds = input.nextInt();
+
+		int minutes = seconds / 60;
+		int remainingSeconds = seconds % 60;
+		System.out.println(seconds + " seconds is " + minutes +
+				" minutes and " + 	remainingSeconds + " seconds.");
+
+	}
+}

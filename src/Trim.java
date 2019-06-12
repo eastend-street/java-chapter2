a4cfb55610fd7ddbb84f9143d5784edb9723fc3c
eastend-street <ss_1322irf@icloud.com>
2019-06-12 15:34:00 -0700

    made contain check program

---
+++ src/Trim.java
@@ -0,0 +1,8 @@
+
+public class Trim {
+	public static void main(String[] args) {
+		String str = new String("         Welcome to Java Class.      ");
+		System.out.print("Return Value");
+		System.out.println(str.trim());
+	}
+}

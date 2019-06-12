b8ec4dce8f4da72caa01710d52d1a68c601dcc81
eastend-street <ss_1322irf@icloud.com>
2019-06-12 14:43:34 -0700

    made contain check program

---
+++ src/ContainsTest.java
@@ -0,0 +1,11 @@
+
+public class ContainsTest {
+	public static void main(String[] args) {
+		String str = "Seoul is the capital city of Korea";
+		if (str.contains("city")) {
+			System.out.println("This string contains city");
+		} else {
+			System.out.println("Result not found");
+		}
+	}
+}
